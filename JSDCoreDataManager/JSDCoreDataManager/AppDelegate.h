3f43f9a6d1d55c0a3c7b4f988d6c3fdf25fae853
姜守栋 <18511632756@163.com>
2017-08-31 19:00:36 +0800

    创建项目

---
+++ JSDCoreDataManager/JSDCoreDataManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSDCoreDataManager
+//
+//  Created by Abner on 2017/8/31.
+//  Copyright © 2017年 姜守栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSDCoreDataManager/JSDCoreDataManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSDCoreDataManager
+//
+//  Created by Abner on 2017/8/31.
+//  Copyright © 2017年 姜守栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
